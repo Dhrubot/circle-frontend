c513c809e1cf73733999d1664be565f4dec11627
Dhrubot <dhrubo.dh@gmail.com>
2020-09-19 20:18:30 -0400

    Added the reducer for grabbign the post of the followings of user

---
+++ src/actions/userFollowingsPosts.js
@@ -0,0 +1,97 @@
+export const setUserFollowingsPosts = posts => {
+    return {
+        type: 'SET_USER_FOLLOWINGS_POST',
+        payload: posts
+    }
+}
+
+export const addNewLike = post => {
+    return {
+        type: 'ADD_NEW_LIKE',
+        payload: post
+    }
+}
+
+export const addNewComment = comment => {
+    return {
+        type: 'ADD_NEW_COMMENT',
+        payload: comment
+    }
+}
+
+export const getUserFollowingsPosts = (id) => {
+    return dispatch => {
+        return fetch(`http://localhost:3001/api/v1/users/${id}/posts`, {
+            credentials: 'include',
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+        })
+            .then(res => res.json())
+            .then(posts => {
+                if (posts.error) {
+                    alert(posts.error)
+                } else {
+                    dispatch(setUserFollowingsPosts(posts))
+                }
+            })
+            .catch(console.log)
+    }
+}
+
+
+export const createNewLike = (postId, likerId, history) => {
+    const likeData = {
+        post_id: postId,
+        liker_id: likerId
+    }
+    return dispatch => {
+        return fetch(`http://localhost:3001/api/v1/posts/${postId}/likes`, {
+            credentials: 'include',
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(likeData)
+        })
+            .then(res => res.json())
+            .then(like => {
+                if (like.error) {
+                    alert(like.error)
+                } else {
+                    dispatch(addNewLike(like))
+                    history.push(`/followings/posts/${like.post_id}`)
+                }
+            })
+            .catch(console.log)
+    }
+}
+
+export const createNewComment = (postId, commentorId, formData) => {
+    console.log('from followings posts comment action')
+    const commentData = {
+        post_id: postId,
+        commentor_id: commentorId,
+        body: formData
+    }
+    return dispatch => {
+        return fetch(`http://localhost:3001/api/v1/posts/${postId}/comments`, {
+            credentials: 'include',
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(commentData)
+        })
+            .then(res => res.json())
+            .then(comment => {
+                if (comment.error) {
+                    alert(comment.error)
+                } else {
+                    dispatch(addNewComment(comment))
+                }
+            })
+            .catch(console.log)
+    }
+}
