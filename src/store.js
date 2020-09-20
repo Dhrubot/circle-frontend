6e41a906a1a87212e62526bfa8a209ffd89edeb4
Dhrubot <dhrubo.dh@gmail.com>
2020-09-19 20:30:38 -0400

    added new reducer for userFollowings

--- src/store.js
+++ src/store.js
@@ -1,20 +1,23 @@
 import { createStore, applyMiddleware, compose, combineReducers } from 'redux'
 import thunk from 'redux-thunk'
-import users from './reducers/users'
 import currentUser from './reducers/currentUser'
 import loginFormData from './reducers/loginFormData'
 import signupFormData from './reducers/signupFormData'
 import newPostFormData from './reducers/newPostFormData'
+import userPosts from './reducers/userPosts'
+import userFollowingsPosts from './reducers/userFollowingsPosts'
 
 const reducer = combineReducers({
-    users,
     currentUser,
+    userPosts,
+    userFollowingsPosts,
     loginFormData,
     signupFormData,
     newPostFormData
 })
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const composeEnhancers = (window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__  && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({ trace: true, traceLimit: 25 })) || compose;
+
 
 const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)))
 
